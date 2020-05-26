1ad182e5548e95697628076b2198f9229123ce1d
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2020-05-26 13:43:24 -0500

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -4,35 +4,23 @@
  * Read more at https://makecode.microbit.org/blocks/custom
  */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
+
 
 /**
- * Custom blocks
+ * Anmashon blocks
  */
-//% weight=100 color=#0fbc11 icon=""
-namespace custom {
+//% weight=80 color=#0fbc11 icon=""
+namespace blinker {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     *blink a led at a prticrler intvel
+     * 
      */
-    //% block="do stuff with $n and $s and $e"
-    export function dostuff(n: number, s: string, e: MyEnum): void {
-        // Add code here
+    //% block="blink at x $x y $y every $interva ms"
+    export function blink(x:number ,y: number, interva: number): void {
+        let sprite = game.createSprite(x, y)
+        sprite.setBlink(interva)
+
     }
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block="conpute fid with $value"
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
+  
 }
